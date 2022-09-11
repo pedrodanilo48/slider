8b23d9f699134f8ab3195076a3d52cb9f01a5dd2
pedrodanilo48 <45889462+pedrodanilo48@users.noreply.github.com>
2022-09-10 23:18:41 -0300

    Add files via upload

---
+++ aedd.js
@@ -0,0 +1,28 @@
+//for (let i = 0; i < 5 ; i++) {
+  //  console.log('loop' + i);
+//}
+//const nomes = ["carlos", "pedro", "marta", "rita"];
+//for (let marcador = 0; marcador < nomes.length; marcador++){
+ //   console.log(nomes[marcador]);
+//}
+//const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+//for (let text = 0; text < cars.length; text++){
+   // console.log(cars[text]);
+//}[]
+const buttons = document.querySelectorAll("[data-carousel-button]") 
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset 
+    if (newIndex < 0) newIndex = slides.children.length -1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
